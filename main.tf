6f844ed33cab35c18ddecd180684da1eed08f7a8
Lars Magelssen <lars@magelssen.io>
2025-08-15 12:59:34 +0200

    added buildspec.yml

--- main.tf
+++ main.tf
@@ -2,8 +2,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "terraform-data-xyz"
+    key          = "state-files/crc-backend.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
   }
 }
 
